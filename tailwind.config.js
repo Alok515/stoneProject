2fac7dc2342f19b57661dcfddf712f26f5872fa9
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2026-02-19 01:43:32 +0530

    Feat: Nuxt3 with Tailwind Setup Done

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'brand-primary': '#222222',
+        'brand-secondary': '#4F4F4F',
+        'brand-light': '#BFBFBF',
+        'brand-border': '#E5E5E5',
+        'brand-danger': '#C5221F',
+        'brand-bg-light': '#F5F5F5',
+      },
+      fontFamily: {
+        sans: ['Urbanist', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
