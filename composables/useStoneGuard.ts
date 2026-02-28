dd2acdc96fd246fd6c6234745f31b443d5d5e92f
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2026-02-28 17:20:30 +0530

    modified: Data Fetching to Server at useStoneGuard

--- composables/useStoneGuard.ts
+++ composables/useStoneGuard.ts
@@ -1,18 +1,25 @@
+import { stoneAttributes } from  "~/data/stoneAttributes"
 export const useStoneGuard = (type: "diamond" | "gemstone") => {
   const stoneAttributesData = ref<any>(null);
   const loadError = ref<string | null>(null);
 
-  onMounted(async () => {
-    try {
-      const mod = await import("~/data/stoneAttributes");
-      stoneAttributesData.value = mod.stoneAttributes;
-    } catch (e) {
-      loadError.value = "Failed to load stone attributes configuration.";
-    }
-  });
+  stoneAttributesData.value = stoneAttributes || [];
+
+  // (async () => {
+  //   try {
+  //     const mod = await import("~/data/stoneAttributes");
+  //     stoneAttributesData.value = mod.stoneAttributes;
+  //   } catch (e) {
+  //     loadError.value = "Failed to load stone attributes configuration.";
+  // }})();
+  // onMounted(async () => {
+  // });
 
   const schema = computed(() => {
-    if (!stoneAttributesData.value || typeof stoneAttributesData.value !== "object") return [];
+    if (!stoneAttributesData.value || typeof stoneAttributesData.value !== "object") {
+      loadError.value = "Stone Attributes is Empty";
+      return [];
+    };
     const attr = stoneAttributesData.value[type];
     return Array.isArray(attr) ? attr : [];
   });
